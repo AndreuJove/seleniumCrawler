bfe44a7be96f15edb2e43529685df3a5328610fd
AndreuJove <andreujove@gmail.com>
2020-11-07 17:01:07 +0100

    Added logging, argparse, exceptions

--- crawler_selenium.py
+++ crawler_selenium.py
@@ -1,36 +1,86 @@
-from selenium import webdriver
+import os
 import time
 import json
+import logging
+from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-#Log for catching exceptions during crawling:
-logf = open("download.log", "w")
+os.environ['WDM_LOG_LEVEL'] = '0'
 
-def get_HTML_document_with_JS(args, tool):
-    #Instance class webdriver with ChromeDriverManager to get the latest version of the WebDriver:
+"""
+Functions of crawler and managing data files
+"""
+
+#Save the item in manifest, for a posterior searching
+def save_item_in_manifest(item, path):
+    data = load_json(path)
+    data['tools_ok_js'].append(item)
+    write_json(data, path)
+
+#This function is called ones to create the template of the output file of the crawler
+def create_file_manifest(path):
+    list_tools = []
+    final_dict = {"tools_ok_js" : list_tools}
+    write_json(final_dict, path)
+    
+#Write on a json file. Input: data and path of the file.
+def write_json(data, path):
+    with open(path, 'w') as file:
+        json.dump(data, file)
+
+#Load json file return the data. Input: and path of the file.
+def load_json(path_file):
+    with open(path_file) as output:
+        return json.load(output)
+
+def get_html_document_with_js(args, tool):  
+    print(f"INFO: Scraping {tool['final_url']}")
     driver = webdriver.Chrome(ChromeDriverManager().install())
+
+    #Create a empty key value to catch the exception of the website
+    tool['exception_selenium'] = ""
     #Request the website if it's available
     try:
-        driver.get(tool['final_url_tool'])
+        driver.get(tool['final_url'])
     #Catch the exception and write on the log file:
-    except Exception as a:
-        logf.write("Failed in request of the tool {0}: {1}\n".format(str(tool), str(a)))
+    except Exception as exception:
+        tool['exception_selenium'] = f"Get fail in {tool['final_url']}. Exception: {str(exception)}"
         html = ""
     #Seconds to wait for the renderitzation of JavaScript of the website
     time.sleep(5)
     #Extract all the HTML from the website if it's possible
     try:
         html = driver.execute_script("return document.documentElement.outerHTML;")
-    #Catch the exception and write on the log file:
-    except Exception as e: 
-        logf.write("Failed to return the HTML in tool {0}: {1}\n".format(str(tool), str(e)))
+    #Catch the exception and write on the key exception_selenium of the tool:
+    except Exception as exception:
+        tool['exception_selenium'] = "Fail return HTML in {tool['final_url']}. Exception: {str(exception)}"
         html= ""
+    #Close the driver
     driver.close()
+
     #Get the last part of the id to name the file of the HTML
-    unique_id = tool['idTool'].split("/")[-1]
-    if html:
-        with open(f"{args.output_directory_htmls}/{unique_id}.json", 'w') as f:
-            json.dump([{'final_url_tool' : tool['final_url_tool']}, {'html_js' : html}], f)
+    html_item = {
+                    "id" : tool['id'],
+                    "html_js" : html
+                }
+
+    #Create the new path for acces the HTML with JS of the URL.
+    tool['path_html_js'] = tool['path_file'].replace("no_", "")
+
+    #Write the item with the id and the html:
+    write_json(html_item, f"{args.o_directory_htmls_js}/{tool['path_html_js']}")
+
+    #Create the path for the manifest of the output of the program
+    path_file_manifest = f"{args.o_directory_data}/{args.output_file}.json"
+
+    #Try to load the the manifest file
+    if not os.path.isfile(path_file_manifest):
+        create_file_manifest(path_file_manifest)
+
+    #Save the new item of the crawler to the manifest json
+    save_item_in_manifest(tool, path_file_manifest)
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,49 +1,89 @@
 import argparse
 import json
 import time
-from multiprocessing import Pool
-from crawler_selenium import get_HTML_document_with_JS
 import os
 from functools import partial
+from multiprocessing import Pool
+from crawler_selenium import get_html_document_with_js
 
+"""
+
+Python Package from crawling websites and saving his HTML with rendered JavaScript.
+
+"""
 
 if __name__ == "__main__":
 
     start = time.time()
+
     # Instance of the class ArgumentParser:
     parser = argparse.ArgumentParser(
         description="Crawler for dynamic websites (use of JavaScript) of a set of bioinformatics tools with")
 
     # Input file for crawling this package: Each tool has: ['name'], ['id'] and ['first_url_tool'] URL of the api to extract the data:
-    parser.add_argument('-i_path_file', '--input_path_list_tools', type=str, 
-                    default="input_data/tools_for_crawling_js.json",
-                    help="File of tools for crawling. Each tool has: ['id'] and ['final_url_tool']")
+    parser.add_argument(
+                        '-input_path_file',
+                        type=str,
+                        default="../mastercrawlerTFG/output_data/manifest_tools.json",
+                        help="File input path")
 
+    # Add the argument of the key acces of the input file:
+    parser.add_argument(
+                        '-key_access',
+                        type=str,
+                        default='tools_ok',
+                        help="Name of the output key to access for the htmls from crawler. Default: tools_ok"
+                        )
     # Add the argument of the ouput directory for htmls_js:
-    parser.add_argument('-o_directory_htmls_js', '--output_directory_htmls', type=str,
-                    default="htmls_js", help="Name of the output stats file from crawler")
-    
+    parser.add_argument(
+                        '-o_directory_htmls_js',
+                        type=str,
+                        default="htmls_js",
+                        help="Name of the output directory for the htmls from crawler. Default: htmls_js"
+                        )
+
+    #Add the argument of the ouput directory for htmls_js:
+    parser.add_argument(
+                        '-o_directory_data',
+                        type=str,
+                        default="output_data_selenium",
+                        help="Name of the output directory manifest ouput file, that gives the path to each tool. Default: output_data"
+                        )
+
+    #Add the argument of the output file:                 
+    parser.add_argument(
+                        '-output_file',
+                        type=str,
+                        default="manifest_tools_js",
+                        help="Name of the output manifest file of the of selenium crawler. Default is: manifest_tools_js"
+                        )
+
     args = parser.parse_args()
 
     #Open json file and transformed to a list of dictionaries.
     #Each dictionary has 2 keys: 'final_url_tool' and 'idTool'.
-    with open(args.input_path_list_tools, "r") as fp:
+    with open(args.input_path_file, "r") as fp:
         tools_to_crawl = json.load(fp)
 
-    # Check if the directory exists and if not exists create it:
-    if not os.path.isdir(args.output_directory_htmls):
-        os.mkdir(args.output_directory_htmls)
+    # Check if the directory for output_data exists and if not exists create it:
+    if not os.path.isdir(args.o_directory_data):
+        os.mkdir(args.o_directory_data)
+    
+    #Check if the directory for HTMLs exists and if not exists create it:
+    if not os.path.isdir(args.o_directory_htmls_js):
+        os.mkdir(args.o_directory_htmls_js)
 
     # Pass args as arguments to pool:
-    func = partial(get_HTML_document_with_JS, args)
+    func = partial(get_html_document_with_js, args)
 
-    #Instance class Pool with 12 processes simultanously:      
-    pool = Pool(processes=12)
+    print(f"INFO: Starting the crawler of {len(tools_to_crawl[args.key_access])}, this can take aproach 2h30minutes of execution.")
+    #Instance class Pool with 12 processes simultanously:
+    pool = Pool(processes=2)
     #Call the function and passs the data to crawl:
-    pool.map(func, tools_to_crawl[:14])
+    pool.map(func, tools_to_crawl[args.key_access][:2])
 
 
     #print Time of execution of the crawler:
     end = time.time()
-    print("\n\nTime of execution:")
+    print("\n\nTime of execution of the crawler:")
     print(end - start)
